d0e341d98ef8206e7e79fdbd5a1c79c17109a3f3
Joan Marie Verba <verba001@umn.edu>
2018-03-14 16:08:31 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+"use strict"
+
+
+let correctAnswers = 0;
+let incorrectAnswers = 0;
+let unanswered = 0;
+
+
+
+
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
+document.onkeyup = function (event) {
+
+    // Captures the key press, converts it to lowercase, and saves it to a variable.
+    var letter = String.fromCharCode(event.which).toLowerCase();
+
+
+
+}
